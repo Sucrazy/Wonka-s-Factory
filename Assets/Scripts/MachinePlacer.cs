f453498417c44e4c9b216987cdd80e5d320b37af
PrivateSkittles <u1008246@utah.edu>
2018-03-27 10:37:27 -0600

    Candy changes on hitting machine

---
+++ Assets/Scripts/MachinePlacer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class MachinePlacer : MonoBehaviour
+{
+    private GridSystem grid;
+    Path pathList;
+    public GameObject prefab;
+
+    void Start() //finds path
+    {
+        pathList = GameObject.FindGameObjectWithTag("Pather").GetComponent<Path>();
+    }
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<GridSystem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) //right click
+        {
+            RaycastHit hitInfo; //raycast finds click information
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceMachineNear(hitInfo.point); //place machine near click
+            }
+        }
+    }
+
+    private void PlaceMachineNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        finalPosition.y = 0.5f;
+        if (Physics.CheckSphere(finalPosition, 0.1f) != true)
+        {
+            var lastObject = pathList.path[pathList.path.Count - 2];
+            if (Vector3.Distance(lastObject.position, finalPosition) <= 1.1f) //check if next machine is close enough
+            {
+                GameObject obj;
+                if (lastObject.position.z != finalPosition.z) //determintes orientation of machine and instantiates it
+                {
+                    obj = GameObject.Instantiate(prefab, finalPosition, Quaternion.Euler(0, 0, 0));
+                }
+                else
+                {
+                    obj = GameObject.Instantiate(prefab, finalPosition, Quaternion.Euler(0, 90f, 0));
+                }
+
+                obj.transform.name = "ConveyerBelt";
+                obj.transform.parent = GameObject.Find("Path").transform; //places object in hierarchy
+                pathList.path.Insert(pathList.path.Count - 1, (obj.transform)); //adds object to path
+            }
+        }
+    }
+}
--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -9,6 +9,8 @@ public class PathFollower : MonoBehaviour {
 	public int currentPoint;
     public Path pathList;
 
+    public GameObject strawberry;
+    public GameObject cockroach;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +20,7 @@ public class PathFollower : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		{
+            GameObject obj = pathList.path[currentPoint].gameObject;
 			float dist = Vector3.Distance (pathList.path[currentPoint].position, transform.position);
 
             Vector3 moveTo = pathList.path[currentPoint].position;
@@ -26,8 +29,40 @@ public class PathFollower : MonoBehaviour {
             if (dist <= 1.5) //distance between two machines isn't too large
 			    transform.position = Vector3.MoveTowards (transform.position, moveTo, speed);
 
-			if (dist == reachDist) //start moving to next machine
-				currentPoint++;
+            if (dist == reachDist) //start moving to next machine
+            {
+                if (obj.CompareTag("Path") || obj.CompareTag("PathCorner"))
+                    currentPoint++;
+
+                else if (obj.CompareTag("Zapper"))
+                {
+                    switch (gameObject.tag)
+                    {
+                        case "Strawberry":
+                            GameObject candyChange = GameObject.Instantiate(cockroach, gameObject.transform.position, gameObject.transform.rotation);
+                            PathFollower cChange = candyChange.GetComponent<PathFollower>();
+                            cChange.currentPoint = currentPoint + 1;
+                            Destroy(this.gameObject);
+                            break;
+
+                        case "Cockroach":
+                            candyChange = GameObject.Instantiate(strawberry, gameObject.transform.position, gameObject.transform.rotation);
+                            cChange = candyChange.GetComponent<PathFollower>();
+                            cChange.currentPoint = currentPoint + 1;
+                            Destroy(this.gameObject);
+                            break;
+
+                        default:
+                            Debug.Log("Nothing");
+                            currentPoint++;
+                            break;
+                    }
+                }
+
+                else
+                    currentPoint++;
+
+            }
 		}
 	}
 }
